f93102876362eb1d674daf01180c2f943a728921
damienakap <32419883+damienakap@users.noreply.github.com>
2021-11-18 14:01:53 -1000

    remove script in html file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,22 @@
 
 var header, navbar_wrapper, navbar, sticky;
 
+var navbar_home, navbar_about, navbar_contact;
+
+var path, page;
+
+function navigate( loc )
+{
+	navbar_home.classList.remove("navbar_current");
+	navbar_about.classList.remove("navbar_current");
+	navbar_contact.classList.remove("navbar_current");
+	
+	if(loc==navbar_home) {navbar_home.classList.add("navbar_current");return;}
+	if(loc==navbar_about) {navbar_about.classList.add("navbar_current");return;}
+	if(loc==navbar_contact) {navbar_contact.classList.add("navbar_current");return;}
+
+}
+
 function stickyNavbar() {
   if (window.pageYOffset - 1 > sticky) {
 		header.classList.add("sticky_header")
@@ -16,11 +32,26 @@ function stickyNavbar() {
 function main()
 {
 	console.log("Started");
+	navbar_home = document.getElementById("navbar_home");
+	navbar_about = document.getElementById("navbar_about");
+	navbar_contact = document.getElementById("navbar_contact");
+
+	path = window.location.pathname;
+	page = path.split("/").pop();
+	console.log( page );
+
+	if(page=="index.html" || page=="") navbar_home.classList.add("navbar_current");
+	if(page=="about.html") navbar_about.classList.add("navbar_current");
+	if(page=="contact.html") navbar_contact.classList.add("navbar_current");
+
+
 	header = document.getElementById("header");
 	navbar_wrapper = document.getElementsByClassName("navbar-wrapper")[0];	// Get the navbar
 	navbar = document.getElementById("navbar");
+
  	sticky = navbar.offsetTop;	// Get the offset position of the navbar
-	 window.onscroll = function() {stickyNavbar()};
+
+	window.onscroll = function() {stickyNavbar()};
 }
 
 window.addEventListener("load",main);
